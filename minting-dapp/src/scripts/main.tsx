1b4030379a36d94bd72ff8a79bb3d0a0a96c9d00
17-Wayne <104151775+17-Wayne@users.noreply.github.com>
2023-02-24 12:58:55 +0800

    ninja

--- minting-dapp/src/scripts/main.tsx
+++ minting-dapp/src/scripts/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import Dapp from './react/Dapp';
 import CollectionConfig from '../../../smart-contract/config/CollectionConfig';
 import CountDown from './react/CountDown';
+import ReactGA from 'react-ga4';
 
 if (document.title === '') {
   document.title = CollectionConfig.tokenName;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
+  ReactGA.initialize('G-9T29FCT0HN');
+
   ReactDOM.render(<>
     <CountDown />
     {/* <Dapp /> */}
